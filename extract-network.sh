ababc55ade5f0fa4586de62f55d010846120a4c4
fboltz <frederic.boltz@gmail.com>
2019-01-12 23:52:59 +0100

    Run on Linux

--- extract-network.sh
+++ extract-network.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ $(uname -s) == "Linux" ]; then
+    DBASE64="base64 -d"
+else
+    DBASE64="base64 -D"
+fi
+
 govc vm.info -json afp-slyo-k8s-autoscaled-test \
     | jq '.VirtualMachines[0].Config.ExtraConfig[] | select (.Key == "guestinfo.metadata")'.Value \
     | tr -d '"' \
-    | base64 -D \
+    | $DBASE64 \
     | gzip -d \
     | jq .network | tr -d '"' \
-    | base64 -D \
+    | $DBASE64 \
     | gzip -d \
 
 
