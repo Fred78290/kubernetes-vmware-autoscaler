414608504ee209f296c8dc805fed58cdb595f12f
Frederic BOLTZ <frederic.boltz@gmail.com>
2019-01-27 10:33:05 +0100

    Remove also machine-id

--- masterkube/guestinfos/cloud-init-clean.sh
+++ masterkube/guestinfos/cloud-init-clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 [ -f /etc/cloud/cloud.cfg.d/50-curtin-networking.cfg ] && rm /etc/cloud/cloud.cfg.d/50-curtin-networking.cfg
 rm /etc/netplan/*
+rm /etc/machine-id
 cloud-init clean
 rm /var/log/cloud-ini*
 rm /var/log/syslog
