eb35a44f036863dd76dd538c9467b4928d373f32
fboltz <frederic.boltz@gmail.com>
2020-09-12 16:58:29 +0200

    FEAT: Add better cloud-init machine-id cleanup

--- masterkube/bin/create-image.sh
+++ masterkube/bin/create-image.sh
@@ -321,8 +321,19 @@ SHELL
 [ -f /etc/cloud/cloud.cfg.d/50-curtin-networking.cfg ] && rm /etc/cloud/cloud.cfg.d/50-curtin-networking.cfg
 rm /etc/netplan/*
 cloud-init clean
-rm /var/log/cloud-ini*
-rm /var/log/syslog
+
+rm -rf /etc/apparmor.d/cache/* /etc/apparmor.d/cache/.features
+/usr/bin/truncate --size 0 /etc/machine-id
+rm -f /snap/README
+find /usr/share/netplan -name __pycache__ -exec rm -r {} +
+rm -rf /var/cache/pollinate/seeded /var/cache/snapd/* /var/cache/motd-news
+rm -rf /var/lib/cloud /var/lib/dbus/machine-id /var/lib/private /var/lib/systemd/timers /var/lib/systemd/timesync /var/lib/systemd/random-seed
+rm -f /var/lib/ubuntu-release-upgrader/release-upgrade-available
+rm -f /var/lib/update-notifier/fsck-at-reboot /var/lib/update-notifier/hwe-eol
+find /var/log -type f -exec rm -f {} +
+rm -r /tmp/* /tmp/.*-unix /var/tmp/*"
+/bin/sync
+/sbin/fstrim -v /
 EOF
 
 chmod +x "${ISODIR}/prepare-image.sh"
