64cab2f917884b6185f49ff9615e1fae7cae6959
fboltz <frederic.boltz@gmail.com>
2022-01-22 15:21:08 +0100

    FEAT: Make easier sources comparaison with aws project

--- server/server.go
+++ server/server.go
@@ -32,6 +32,9 @@ type applicationInterface interface {
 var availableGPUTypes = map[string]string{
 }
 
+var availableGPUTypes = map[string]string{
+}
+
 // AutoScalerServerApp declare AutoScaler grpc server
 type AutoScalerServerApp struct {
 	ResourceLimiter *types.ResourceLimiter                `json:"limits"`
